ebed72979d481ca1acde87a6d860e570f694aa62
William Roe <willroe@gmail.com>
2012-08-23 23:28:21 +0100

    add piece rotations and keypresses rotate the pieces

--- src/cljs/caves/game.cljs
+++ src/cljs/caves/game.cljs
@@ -7,42 +7,50 @@
 (def sq-width 30)
 (def sq-height 30)
 
+(defn grid-to-dimentions
+  "Take grid coords, [0 0], [0 2] and make coords out of them for the squares"
+  [[nx ny]]
+  [(* sq-width nx)
+   (* sq-height ny)])
+
+(defn rotations-to-dimentions
+  [rotations]
+  (map grid-to-dimentions rotations))
+
 (def shapes
   {:o
-   [[0 0]
-    [sq-height sq-width]
-    [0 sq-height]
-    [sq-width 0]]
+   (map rotations-to-dimentions
+        [[[0 0] [1 1] [0 1] [1 0]]])
    :j
-   [[0 (* 2 sq-height)]
-    [sq-width 0]
-    [sq-width sq-height]
-    [sq-width (* 2 sq-height)]]
+   (map rotations-to-dimentions
+        [[[0 2] [1 0] [1 1] [1 2]]
+         [[0 0] [0 1] [1 1] [2 1]]
+         [[0 0] [0 1] [0 2] [1 0]]
+         [[0 0] [1 0] [2 0] [2 1]]])
    :i
-   [[0 0]
-    [0 sq-height]
-    [0 (* 2 sq-height)]
-    [0 (* 3 sq-height)]]
+   (map rotations-to-dimentions
+        [[[0 0] [0 1] [0 2] [0 3]]
+         [[0 0] [1 0] [2 0] [3 0]]])
    :l
-   [[0 0]
-    [0 sq-height]
-    [0 (* 2 sq-height)]
-    [sq-width (* 2 sq-height)]]
+   (map rotations-to-dimentions
+        [[[0 0] [0 1] [0 2] [1 2]]
+         [[0 0] [1 0] [2 0] [0 1]]
+         [[0 0] [1 0] [1 1] [1 2]]
+         [[0 1] [1 1] [2 1] [2 0]]])
    :s
-   [[0 sq-height]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) 0]]
+   (map rotations-to-dimentions
+        [[[0 1] [1 0] [1 1] [2 0]]
+         [[0 0] [0 1] [1 1] [1 2]]])
    :z
-   [[0 0]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) sq-height]]
+   (map rotations-to-dimentions
+        [[[0 0] [1 0] [1 1] [2 1]]
+         [[1 0] [1 1] [0 1] [0 2]]])
    :t
-   [[0 0]
-    [sq-width 0]
-    [sq-width sq-height]
-    [(* 2 sq-width) 0]]})
+   (map rotations-to-dimentions
+        [[[0 0] [1 0] [1 1] [2 0]]
+         [[0 1] [1 0] [1 1] [1 2]]
+         [[0 1] [1 0] [1 1] [2 1]]
+         [[0 0] [0 1] [0 2] [1 1]]])})
 
 (defn get-shape
   "Get the co-ordinates of a shape. Either returns the required shape or a random shape."
@@ -57,6 +65,7 @@
   {:origin origin
    :block-width sq-width
    :block-height sq-height
+   :rotation 0
    :blocks
    (apply get-shape shape-name)})
 
@@ -90,17 +99,33 @@
     (. surface (closePath))
     (. surface (fill))))
 
+(defn draw-piece
+  [surface piece]
+  (let [{:keys [origin blocks rotation]} piece]
+    (let [blocks (nth blocks rotation)]
+      (.log js/console (format "rotation: %s blocks: %s" rotation blocks))
+      (doseq [[incx incy] blocks]
+        (let [x (+ incx (:x origin))
+              y (+ incy (:y origin))]
+          (fill-rect surface [x y sq-width sq-height] [60 80 160])
+          (stroke-rect surface [x y sq-width sq-height] 2 [0 0 0]))))))
+
 (defn update-canvas
   [world surface]
   (let [falling-shapes (:falling world)
         [_ width height] surface]
     (fill-rect surface [0 0 width height] [10 10 10])
     (stroke-rect surface [0 0 width height] 2 [0 0 0])
-    (doseq [{:keys [origin blocks]} falling-shapes]
-      (doseq [[incx incy] blocks]
-        (let [x (+ incx (:x origin))
-              y (+ incy (:y origin))]
-          (fill-rect surface [x y sq-width sq-height] [60 80 160]))))))
+    (doseq [piece falling-shapes]
+      (draw-piece surface piece))))
+
+(defn rotate-piece
+  [piece]
+  (update-in piece [:rotation] #(mod (inc %) (count (:blocks piece)))))
+
+(defn rotate-pieces
+  [pieces]
+  (map rotate-piece pieces))
 
 (defn move-pieces-down
   [state [_ width height]]
@@ -118,8 +143,14 @@
                        )))))
 
 (defn click [timer state surface event]
-  (when (not (.-enabled timer))
-    (. timer (start))))
+  (if (not (.-enabled timer))
+    (. timer (start))
+    (. timer (stop))))
+
+(defn keypress
+  [state]
+  (swap! state (fn [curr]
+                 (update-in curr [:falling] rotate-pieces))))
 
 (defn ^:export main
   []
@@ -128,4 +159,5 @@
         state (atom {:falling [(make-shape {:x 30 :y 0})]})]
     (update-canvas @state surface)
     (events/listen timer goog.Timer/TICK #(game timer state surface))
+    (events/listen js/window event-type/KEYPRESS #(keypress state))
     (events/listen js/window event-type/CLICK #(click timer state surface %))))
